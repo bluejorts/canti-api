f4b077d4b4e5ec5eed8df2eecb2760b894a4f461
Barry Bluejorts <austin.is.paxton@gmail.com>
2023-03-29 00:51:06 -0700

    swap models

--- pages/api/canti.ts
+++ pages/api/canti.ts
@@ -83,7 +83,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     try {
       const completion = await openai.createChatCompletion({
-        model: 'gpt-4',
+        model: 'gpt-3.5-turbo',
         messages: session.messages
       });
 
